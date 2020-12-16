e223bdffa4b96b0701640062d418581f78f742cd
Mladen Srdić <m.s.qubits@gmail.com>
2020-12-16 23:19:43 +0100

    Simple handling of hsdeps dot export.

--- src/Main.hs
+++ src/Main.hs
@@ -15,13 +15,14 @@ main = do
     "sdeps":"list":_ -> handleCommand (StackDeps List)
     "sdeps":"check":_ -> handleCommand (StackDeps Check)
     "hsdeps":"preview":_ -> handleCommand (HSDeps Preview)
+    "hsdeps":"dot":_ -> handleCommand (HSDeps Dot)
     _ -> usage
 
 handleCommand :: Command -> IO ()
 handleCommand (StackDeps List) = error "not implemented"
 handleCommand (StackDeps Check) = error "not implemented"
 handleCommand (HSDeps Preview) = system "hsdeps . | dot -Tpng | open -a Preview.app -f" >> return ()
-handleCommand (HSDeps Dot) = error "not implemented"
+handleCommand (HSDeps Dot) = system "hsdeps ." >> return ()
 
 usage :: IO ()
 usage = do
